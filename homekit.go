4b75ccc450d2dccab8fe6f0c672fc65d580ec41d
Peter Wood <peter@alastria.net>
2025-09-30 21:54:04 +0100

    Add service label after to ensure ID continuity.

--- homekit.go
+++ homekit.go
@@ -85,12 +85,6 @@ func (h *HomeKit) constructServer(buttons []*ButtonDevice) *hap.Server {
 			b.HKBattery = s
 		}
 
-		sl := service.NewServiceLabel()
-		sl.ServiceLabelNamespace.SetValue(characteristic.ServiceLabelNamespaceArabicNumerals)
-		sl.Id = id
-		id++
-		acc.AddS(sl.S)
-
 		for n, bb := range b.Buttons {
 			s := service.NewStatelessProgrammableSwitch()
 			s.Id = id
@@ -125,6 +119,12 @@ func (h *HomeKit) constructServer(buttons []*ButtonDevice) *hap.Server {
 				h.logger.Debug("Publishing button event.", "id", s.Id, "action", new)
 			})
 		}
+
+		sl := service.NewServiceLabel()
+		sl.ServiceLabelNamespace.SetValue(characteristic.ServiceLabelNamespaceArabicNumerals)
+		sl.Id = id
+		id++
+		acc.AddS(sl.S)
 
 		accessories = append(accessories, acc)
 	}
